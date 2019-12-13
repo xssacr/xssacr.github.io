1d5b094af7d2cdeffedb20918bc8f1b84de3f60a
xssacr <xssacr@aliyun.com>
2019-12-13 08:56:33 +0800

    Site updated: 2019-12-13 08:56:32

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -4,13 +4,13 @@ window.onload = function () {
   var titleTime;
   document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-      $('[rel="icon"]').attr('href', "../images/failure-32.ico");
-      $('[rel="shortcut icon"]').attr('href', "../images/failure-32.ico");
+      $('[rel="icon"]').attr('href', "/images/failure-32.ico");
+      $('[rel="shortcut icon"]').attr('href', "/images/failure-32.ico");
       document.title = '要经常回来看看呦~';
       clearTimeout(titleTime);
     } else {
-      $('[rel="icon"]').attr('href', "../images/favicon-32.ico");
-      $('[rel="shortcut icon"]').attr('href', "../images/favicon-32.ico");
+      $('[rel="icon"]').attr('href', "/images/favicon-32.ico");
+      $('[rel="shortcut icon"]').attr('href', "/images/favicon-32.ico");
       document.title = '欢迎回来~';
       titleTime = setTimeout(function () {
         document.title = OriginTitile;
